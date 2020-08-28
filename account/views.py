bbd1dcb011ef6503b3012049b90741b2e092233d
Julio Felipe <julio.felipe@casaldev.com.br>
2020-08-28 19:10:37 -0300

    creating list and detail views for user profiles

--- account/views.py
+++ account/views.py
@@ -2,9 +2,13 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+
+from common.decorator import ajax_required
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
-
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from .models import Contact
 
 @login_required
 def dashboard(request):
@@ -76,3 +80,26 @@ def user_detail(request, username):
                   'account/user/detail.html',
                   {'section': 'people',
                    'user': user})
+
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    print(user_id)
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user,
+                                       user_to=user).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'ko'})
+    return JsonResponse({'status': 'ko'})
